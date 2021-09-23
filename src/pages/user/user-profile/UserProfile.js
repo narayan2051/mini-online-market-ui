f569ff8ac88f0f2158a3432726a2b3c97bf51765
Bipin Karki <bipinkarki@Bipins-MacBook-Pro.local>
2021-09-23 11:34:29 -0500

    Clean User profile

--- src/pages/user/user-profile/UserProfile.js
+++ src/pages/user/user-profile/UserProfile.js
@@ -8,35 +8,10 @@ import Product from "../product/Product";
 // import "./UserProfile.css";
 
 const UserProfile = () => {
-  const [data, setData] = React.useState();
-  // @TODO: user reducers and state
-  React.useEffect(() => {
-    HTTPClient.get(PRODUCT_URL)
-      .then((response) => {
-        console.log(response.data);
-        setData(response.data);
-      })
-      .catch((err) => console.log(err.message));
-  }, []);
-
-  const productList =
-    data &&
-    data.map((item) => {
-      return (
-        <Product
-          id={item.id}
-          title={item.title}
-          price={item.price}
-          description={item.description}
-        />
-      );
-    });
   return (
     <div className="UserProfile">
       <Container maxWidth="sm">
-        {productList}
         <OrderHistory />
-        {/* <Route path="/singleorder/:id" component={SingleOrder} /> */}
       </Container>
     </div>
   );
