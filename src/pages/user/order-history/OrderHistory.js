2d7615897a439b073fe6f35c114ad8f00c61cd11
Bipin Karki <bipinkarki@Bipins-MacBook-Pro.local>
2021-09-23 12:04:27 -0500

    update orderhistory

--- src/pages/user/order-history/OrderHistory.js
+++ src/pages/user/order-history/OrderHistory.js
@@ -1,60 +1,49 @@
 import React, { useState, useEffect } from "react";
-// import { CSVLink } from "react-csv";
-import jsPDF from "jspdf";
-// import { Link } from "react-router-dom";
-import html2canvas from "html2canvas";
+// import jsPDF from "jspdf";
+// import html2canvas from "html2canvas";
 
-import HTTPClient, { PRODUCT_URL } from "../../../api/api";
+import HTTPClient, { ORDER_URL, PRODUCT_URL } from "../../../api/api";
 import { Link } from "react-router-dom";
-import { Container } from "@material-ui/core";
-import { CSVLink } from "react-csv";
-import Product from "../product/Product";
+import {
+  Button,
+  Container,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
+import {
+  Block,
+  Cached,
+  DirectionsBoat,
+  LocalShipping,
+  Store,
+  Title,
+} from "@material-ui/icons";
+// import { CSVLink } from "react-csv";
+
 
 // html2PDF(node, options);
 const OrderHistory = () => {
-  const [data, setData] = useState([
-    {
-      id: 1,
-      title: "Dell",
-      price: 999,
-      description: "Good Laptop",
-    },
-    {
-      id: 1,
-      title: "Dell",
-      price: 999,
-      description: "Good Laptop",
-    },
-  ]);
+  const [data, setData] = useState();
 
   // @TODO: user reducers and state
-
-  useEffect(() => {
-    HTTPClient.get(PRODUCT_URL)
+  const getOrders = () => {
+    HTTPClient.get(ORDER_URL)
       .then((response) => {
         setData(response.data);
       })
       .catch((err) => console.log(err.message));
+  };
+
+  useEffect(() => {
+    getOrders();
   }, []);
-  const productList =
-    data &&
-    data.map((item) => {
-      return (
-        <Product
-          id={item.id}
-          title={item.title}
-          price={item.price}
-          description={item.description}
-        />
-      );
-    });
 
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+  console.log(data)
 
   const csvData = [
     ["firstname", "lastname", "email"],
@@ -64,83 +53,72 @@ const OrderHistory = () => {
   ];
 
   const genereatePDF = () => {
-    var doc = new jsPDF("p", "pt", "a4");
+    // var doc = new jsPDF("p", "pt", "a4");
 
-    doc.html(document.querySelector("#capture"), {
-      callback: function (pdf) {
-        html2canvas(document.querySelector("#capture")).then((canvas) => {
-          pdf.save("download.pdf");
-        });
-      },
-    });
+    // doc.html(document.querySelector("#capture"), {
+    //   callback: function (pdf) {
+    //     // html2canvas(document.querySelector("#capture")).then((canvas) => {
+    //       pdf.save("download.pdf");
+    //     // });
+    //   },
+    // });
   };
 
+  
   return (
-    <div className="OrderHistory">
-      <div>
-        <h1> Order History </h1>
-        <div>
-          <div>Order Id: #111</div>
-          <div>
-            Order status : <button className="primary"> Processing</button>
-          </div>
-          <div>Shipping Address : ada dashboard adasd</div>
-          <div>Amount : $111</div>
-          <div>
-            Seller: <button className="primary"> Follow</button>
-          </div>
-          <div>
-            <Link to="/user/singleorder/?id=1">
-              <button>See More</button>
-            </Link>
-          </div>
-        </div>
-        <div>
-          <div>Order Id: #111</div>
-          <div>
-            Order status : <button className="primary"> Processing</button>
-          </div>
-          <div>Shipping Address : ada dashboard adasd</div>
-          <div>Amount : $111</div>
-          <div>
-            Seller: <button className="primary"> Follow</button>
-          </div>
-        </div>
-        <div>
-          <div>Order Id: #111</div>
-          <div>
-            Order status : <button className="primary"> Processing</button>
-          </div>
-          <div>Shipping Address : ada dashboard adasd</div>
-          <div>Amount : $111</div>
-          <div>
-            Seller: <button className="primary"> Follow</button>
-          </div>
-        </div>
-      </div>
-
-      <div id="capture">
-        <div>
-          <div>Order Id: #111</div>
-          <div>Order status : Delivered</div>
-          <div>Shipping Address : ada dashboard adasd</div>
-          <div>Billing Address : ada dashboard adasd</div>
-          <div>Amount : $111</div>
-          <div>Seller: Bipin Karki</div>
-          <div>{/* <SingleProduct /> */}</div>
-        </div>
-      </div>
-      <div className="export_buttons">
-        <CSVLink data={csvData}>
-          <button>CSV</button>
-        </CSVLink>
-        <button id="btn" onClick={genereatePDF}>
-          PDF
-        </button>
-      </div>
-      <Container maxWidth="sm">OrderHistory</Container>
-    </div>
+    <Container maxWidth="sm">
+      <Title>All Orders</Title>
+      <Table size="small" className="Orders">
+        <TableHead id="table-head">
+          <TableRow>
+            <TableCell>Order Number</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Order Status</TableCell>
+            <TableCell>Billing Address</TableCell>
+            <TableCell>Shipping Adress</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data &&
+            data.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.id}</TableCell>
+                <TableCell>
+                  {new Date(row.createdDate).toLocaleDateString()}
+                </TableCell>
+                <TableCell>${row.amount}</TableCell>
+                <TableCell>{row.orderStatus}</TableCell>
+                <TableCell>{row.billingAddress}</TableCell>
+                <TableCell>{row.shippingAddress}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </Container>
   );
+  //     <div id="capture">
+  //       <div>
+  //         <div>Order Id: #111</div>
+  //         <div>Order status : Delivered</div>
+  //         <div>Shipping Address : ada dashboard adasd</div>
+  //         <div>Billing Address : ada dashboard adasd</div>
+  //         <div>Amount : $111</div>
+  //         <div>Seller: Bipin Karki</div>
+  //         <div>{/* <SingleProduct /> */}</div>
+  //       </div>
+  //     </div>
+  //     <div className="export_buttons">
+  //       {/* <CSVLink data={csvData}>
+  //         <button>CSV</button>
+  //       </CSVLink> */}
+  //       <button id="btn" onClick={genereatePDF}>
+  //         PDF
+  //       </button>
+  //     </div>
+  //     <Container maxWidth="sm">OrderHistory</Container>
+  //   </div>
+  
 };
 
 export default OrderHistory;
